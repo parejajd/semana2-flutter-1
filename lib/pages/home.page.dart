52faf56fbe6a77916eb42385911f9fc19f2274ee
Juan David Pareja Soto <parejajd@outlook.com>
2021-09-16 21:30:10 -0500

    Calculadora realizando operacioens basicas, falta validar errores en las expresiones

--- lib/pages/home.page.dart
+++ lib/pages/home.page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
 import 'package:semanda2flutter1/widgets/button.component.dart';
 
 class HomePage extends StatefulWidget {
@@ -9,26 +10,34 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  String expresion = '0';
+  String resultado = '_';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Home')),
       body: Column(children: [
         Expanded(
-            flex: 5,
+            flex: 3,
             child: Container(
-              child: Row(
-                children: [Text("Zona Resultados")],
+              alignment: Alignment.centerLeft,
+              padding: EdgeInsets.fromLTRB(10, 20, 10, 0),
+              child: Text(
+                resultado,
+                style: TextStyle(fontSize: 38),
               ),
-              color: Colors.blue,
+              //color: Colors.blue,
             )),
         Expanded(
             flex: 1,
             child: Container(
-              child: Row(
-                children: [Text("Zona Operaciones")],
+              alignment: Alignment.centerRight,
+              padding: EdgeInsets.fromLTRB(10, 30, 10, 0),
+              child: Text(
+                expresion,
+                style: TextStyle(fontSize: 38),
               ),
-              color: Colors.red,
+              //color: Colors.red,
             )),
         Expanded(
             flex: 2,
@@ -38,53 +47,185 @@ class _HomePageState extends State<HomePage> {
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceAround,
                     children: [
-                      CalculatorButton(widget: Text("7"), method: () {}),
-                      CalculatorButton(widget: Text("8"), method: () {}),
-                      CalculatorButton(widget: Text("9"), method: () {}),
-                      CalculatorButton(widget: Text("/"), method: () {}),
                       CalculatorButton(
-                          widget: Icon(Icons.arrow_back), method: () {}),
-                      CalculatorButton(widget: Text("C"), method: () {}),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      CalculatorButton(widget: Text("4"), method: () {}),
-                      CalculatorButton(widget: Text("5"), method: () {}),
-                      CalculatorButton(widget: Text("6"), method: () {}),
-                      CalculatorButton(widget: Text("*"), method: () {}),
-                      CalculatorButton(widget: Text("("), method: () {}),
-                      CalculatorButton(widget: Text(")"), method: () {}),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      CalculatorButton(widget: Text("1"), method: () {}),
-                      CalculatorButton(widget: Text("2"), method: () {}),
-                      CalculatorButton(widget: Text("3"), method: () {}),
-                      CalculatorButton(widget: Text("-"), method: () {}),
-                      CalculatorButton(widget: Text("Pow"), method: () {}),
-                      CalculatorButton(widget: Text("Sqrt"), method: () {}),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      CalculatorButton(widget: Text("0"), method: () {}),
-                      CalculatorButton(widget: Text("."), method: () {}),
-                      CalculatorButton(widget: Text("%"), method: () {}),
-                      CalculatorButton(widget: Text("+"), method: () {}),
+                          widget: Text("7"),
+                          method: () {
+                            onPressButton("7");
+                          }),
                       CalculatorButton(
-                          widget: Text("="), method: () {}, flex: 2),
+                          widget: Text("8"),
+                          method: () {
+                            onPressButton("8");
+                          }),
+                      CalculatorButton(
+                          widget: Text("9"),
+                          method: () {
+                            onPressButton("9");
+                          }),
+                      CalculatorButton(
+                          widget: Text("/"),
+                          method: () {
+                            onPressButton("/");
+                          }),
+                      CalculatorButton(
+                          widget: Icon(Icons.arrow_back),
+                          method: () {
+                            onPressButton("b");
+                          }),
+                      CalculatorButton(
+                          widget: Text("C"),
+                          method: () {
+                            onPressButton("c");
+                          }),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      CalculatorButton(
+                          widget: Text("4"),
+                          method: () {
+                            onPressButton("4");
+                          }),
+                      CalculatorButton(
+                          widget: Text("5"),
+                          method: () {
+                            onPressButton("5");
+                          }),
+                      CalculatorButton(
+                          widget: Text("6"),
+                          method: () {
+                            onPressButton("6");
+                          }),
+                      CalculatorButton(
+                          widget: Text("*"),
+                          method: () {
+                            onPressButton("*");
+                          }),
+                      CalculatorButton(
+                          widget: Text("("),
+                          method: () {
+                            onPressButton("(");
+                          }),
+                      CalculatorButton(
+                          widget: Text(")"),
+                          method: () {
+                            onPressButton(")");
+                          }),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      CalculatorButton(
+                          widget: Text("1"),
+                          method: () {
+                            onPressButton("1");
+                          }),
+                      CalculatorButton(
+                          widget: Text("2"),
+                          method: () {
+                            onPressButton("2");
+                          }),
+                      CalculatorButton(
+                          widget: Text("3"),
+                          method: () {
+                            onPressButton("3");
+                          }),
+                      CalculatorButton(
+                          widget: Text("-"),
+                          method: () {
+                            onPressButton("-");
+                          }),
+                      CalculatorButton(
+                          widget: Text("Pow"),
+                          method: () {
+                            onPressButton("p");
+                          }),
+                      CalculatorButton(
+                          widget: Text("Sqrt"),
+                          method: () {
+                            onPressButton("s");
+                          }),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      CalculatorButton(
+                          widget: Text("0"),
+                          method: () {
+                            onPressButton("0");
+                          }),
+                      CalculatorButton(
+                          widget: Text("."),
+                          method: () {
+                            onPressButton(".");
+                          }),
+                      CalculatorButton(
+                          widget: Text("%"),
+                          method: () {
+                            onPressButton("%");
+                          }),
+                      CalculatorButton(
+                          widget: Text("+"),
+                          method: () {
+                            onPressButton("+");
+                          }),
+                      CalculatorButton(
+                          widget: Text("="),
+                          method: () {
+                            onPressButton("=");
+                          },
+                          flex: 2,
+                          colorBoton: Colors.lightGreen),
                     ],
                   ),
                 ],
               ),
-              color: Colors.green,
+              //color: Colors.green,
             )),
       ]),
     );
+  }
+
+  onPressButton(String text) {
+    setState(() {
+      //Se evaluan las opciones de C, Backspace
+      switch (text) {
+        case "c":
+          expresion = '0';
+          break;
+        case "b":
+          if (expresion.length >= 1) {
+            expresion = expresion.substring(0, expresion.length - 1);
+          }
+          if (expresion.length == 0) {
+            expresion = '0';
+          }
+          break;
+        case "=":
+          Parser p = Parser();
+          try {
+            Expression exp = p.parse(expresion);
+
+            ContextModel cm = ContextModel();
+            resultado = '${exp.evaluate(EvaluationType.REAL, cm)}';
+          } catch (e) {
+            resultado = "Error";
+          }
+          break;
+        case "p":
+          break;
+        case "s":
+          break;
+        default:
+          if (expresion == '0') {
+            expresion = text;
+          } else {
+            expresion += text;
+          }
+      }
+    });
   }
 }
--- lib/widgets/button.component.dart
+++ lib/widgets/button.component.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
 
 Widget CalculatorButton(
-    {required Widget widget, required dynamic method, int flex = 1}) {
+    {required Widget widget,
+    required dynamic method,
+    int flex = 1,
+    dynamic colorBoton = Colors.grey}) {
   return Expanded(
     flex: flex,
     child: Padding(
       padding: const EdgeInsets.all(2.0),
       child: Container(
         child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            primary: colorBoton,
+          ),
           onPressed: method,
           child: widget,
+          autofocus: false,
         ),
       ),
     ),
